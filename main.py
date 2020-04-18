8f8e4ac0ad447a5a3be5c04552c0f823880045bc
robbie <robbiekruszynski@gmail.com>
2020-04-18 14:28:21 -0700

    add game loop for window

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ pygame.init()
 
 #screen creation
 screen = pygame.display.set_mode((800,600 ))
+
+#loop to keep the game running unless you click the close button => running == False
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
